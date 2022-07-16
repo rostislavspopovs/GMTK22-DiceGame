ab279d685199a595e1436ff2f0e0ac5b0f122743
Ross Popovs <onyxiastolemylunch@gmail.com>
2022-07-16 17:28:38 +0100

    Commit 4 Ryn

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class GameController : Singleton<GameController>
 {
     [SerializeField] private List<Level> levels;
-
+    [SerializeField] private GameObject dicePrefab;
+    private GameObject dice;
+    private (int,int) dicePos;
+    private int diceFloor;
 
     void Start()
     {
