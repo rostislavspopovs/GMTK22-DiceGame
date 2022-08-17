6434b39150c450e00332ab9abbe2791539e97c2d
Ross Popovs <onyxiastolemylunch@gmail.com>
2022-08-17 14:31:08 +0100

    Update shader

--- Assets/Scripts/PixelImageEffect.cs
+++ Assets/Scripts/PixelImageEffect.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class PixelImageEffect : MonoBehaviour
 {
     public Material EffectMaterial;
+    public int divisor;
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
 
-        RenderTexture RT = RenderTexture.GetTemporary(source.width/2, source.height/2, 0, source.format);
+        RenderTexture RT = RenderTexture.GetTemporary(source.width / divisor, source.height/ divisor, 0, source.format);
         Graphics.Blit(source, RT);
         Graphics.Blit(RT, destination, EffectMaterial);
 
